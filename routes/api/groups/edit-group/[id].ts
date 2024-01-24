8c06cd00da3f7a61aa6a45e3f93fba9c985b1361
Fabián Mendoza <fmendoza.ospina7@outlook.com>
2024-01-23 20:21:50 -0500

    Adding groups

--- routes/api/groups/edit-group/[id].ts
+++ routes/api/groups/edit-group/[id].ts
@@ -4,5 +4,15 @@ import { Group } from "../../../../models/Group.ts";
 export const handler: Handlers = {
   async GET(req, ctx) {
     const group = await Group.findByPk(ctx.params.id);
+
+    if (group) {
+      return new Response(JSON.stringify(group), {
+        status: 200,
+      });
+    }
+
+    return new Response(null, {
+      status: 404,
+    });
   },
 };
